71fac9bd6abef033ffc2e68204a53c04e06bf480
Nilsachy <nils.achy@gmail.com>
2021-12-24 17:07:33 +0100

    Deploy

--- dist/_nuxt/static/1640358851/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ftodo","\u002Finspire","\u002Fmovies","\u002Frestaurants","\u002Fpictures","\u002Fplaces"]})
---
+++ dist/_nuxt/static/1640362043/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ftodo","\u002Fpictures","\u002Frestaurants","\u002Fplaces","\u002Fmovies"]})
